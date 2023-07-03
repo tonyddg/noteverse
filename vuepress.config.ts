8ce1fbe9422ddcbf4a0e0b9dfd5388b25c5a7239
tonyddg <799052200@qq.com>
2023-07-03 23:44:25 +0800

    first commit

---
+++ vuepress.config.ts
@@ -0,0 +1,34 @@
+import { defineUserConfig } from 'vuepress'
+import { hopeTheme } from "vuepress-theme-hope";
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: "Tonyddg's Noteverse",
+    description: 'A place for everythin I would like to record.',
+    base: '/noteverse/',
+
+    theme: hopeTheme({
+        plugins: {
+            mdEnhance:{
+                // 使用 KaTeX 启用 TeX 支持
+                katex: true,
+                // 启用标记
+                mark: true,
+            }
+        },
+        // 页脚设置
+        displayFooter: true,
+        footer: "powered by <a href='https://theme-hope.vuejs.press/zh/'>vuepress-theme-hope</a>",
+        // 其他设置
+        // 仓库地址
+        repo: 'https://github.com/tonyddg/noteverse',
+        // 主要 branch
+        docsBranch: 'master',
+        // 文档目录
+        docsDir: '/docs',
+        //全屏按钮
+        fullscreen: true,
+        //打印按钮
+        print: true,
+    }),
+})
